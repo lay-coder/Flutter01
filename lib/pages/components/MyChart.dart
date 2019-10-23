5df9258b5b4d6c2aefe39cd9b2815a9193c4a5e5
lay-coder <nanmucoffin@163.com>
2019-10-23 15:41:32 +0800

    provider

--- lib/pages/components/MyChart.dart
+++ lib/pages/components/MyChart.dart
@@ -3,60 +3,9 @@ import 'package:flutter01/store/index.dart';
 import 'package:pie_chart/pie_chart.dart';
 import 'package:provider/provider.dart';
 
-// class MyChart extends StatefulWidget {
-//   MyChart({Key key}) : super(key: key);
-
-//   @override
-//   _MyChartState createState() => _MyChartState();
-// }
-
-// class _MyChartState extends State<MyChart> {
-//   Map<String, double> dataMap = new Map();
-//   List<Color> colorList = [
-//     Colors.red,
-//     Colors.green,
-//     Colors.blue,
-//     Colors.yellow,
-//   ];
-//   @override
-//   void initState() {
-//     super.initState();
-//     dataMap.putIfAbsent("Flutter", () => 5);
-//     dataMap.putIfAbsent("React", () => 3);
-//     dataMap.putIfAbsent("Xamarin", () => 2);
-//     dataMap.putIfAbsent("Ionic", () => 2);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer<MySchedule>(
-//       builder: (context, schedule, _) => PieChart(
-//         dataMap: dataMap,
-//         legendFontColor: Colors.blueGrey,
-//         legendFontSize: 14.0,
-//         legendFontWeight: FontWeight.w500,
-//         animationDuration: Duration(milliseconds: 800),
-//         chartLegendSpacing: 32.0,
-//         // chartRadius: MediaQuery.of(context).size.width / 2,
-//         showChartValuesInPercentage: true,
-//         showChartValues: true,
-//         showChartValuesOutside: false,
-//         chartValuesColor: Colors.blueGrey[900].withOpacity(0.9),
-//         colorList: colorList,
-//         showLegends: true,
-//         decimalPlaces: 1,
-//       ),
-//     );
-//   }
-// }
 class MyChart extends StatelessWidget {
-  // const MyChart({Key key}) : super(key: key);
-  List<Color> colorList = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.yellow,
-  ];
+  const MyChart({Key key}) : super(key: key);
+
   static createData(double val) {
     Map<String, double> dataMap = new Map();
     dataMap.putIfAbsent("Flutter", () => val * 10);
@@ -68,6 +17,12 @@ class MyChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Color> colorList = [
+      Colors.red,
+      Colors.green,
+      Colors.blue,
+      Colors.yellow,
+    ];
     return Consumer<MySchedule>(
       builder: (context, schedule, _) => PieChart(
         dataMap: createData(schedule.stateManagementTime),
